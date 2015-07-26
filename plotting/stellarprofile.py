c4e9ab7d20535c2e778a3f096d2c6e00624f0b1e
eml11 <eml11@ic.ac.uk>
2015-07-26 21:04:49 +0100

    added plotting file

---
+++ plotting/stellarprofile.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pylab as pb
+
+inner = np.loadtxt('./stellarstructure_inner.txt',skiprows=2)
+outer = np.loadtxt('./stellarstructure_outer.txt',skiprows=2)[::-1]
+
+data = np.concatenate(inner,outer)
+
+gamma = 5./3.
+
+mass_max = data[-1,0]
+radiusvar_max = data[-1,1]
+pressurevar_max = data[0,2]
+
+Temperature = np.exp(4*(1 - 1/gamma)*(data[:,2] - pressurevar_max))
+
+print
+print 'Stellar Parameters'
+print
+print 'Surface'
+print 'Temperature: ', Temperature[-1]**(1./4.)
+print 'Radius: ', radiusvar_max[-1]**(1./3.)
+print
+print 'Core'
+print 'Temperature: ', Temperature[0]**(1./4.)
+print 'Pressure: ', np.exp(pressurevar_max)
+print
+print 'Error'
+print 'Pressure: ', np.abs((inner[-1,1] - outer[0,1])/inner[-1,1])
+print 'Radius: ',  np.abs((inner[-1,2] - outer[0,2])/inner[-1,2])
+
+
+
+pb.plot(data[:,0]/mass_max,data[:,1]/radiusvar_max,linewidth=2.0,label='Normalised r^3')
+pb.plot(data[:,0]/mass_max,data[:,2]/pressurevar_max,linewidth=2.0,label='Normalised lnP')
+pb.plot(data[:,0]/mass_max,linewidth=2.0,label='Normalised T^4')
+
+pb.xlabel('Normalised Mass')
+
+pb.legend(loc='best')
+pb.show()
+

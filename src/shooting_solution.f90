579bf48689393c6baf2a5868f8f91a8b98955724
eml11 <eml11@ic.ac.uk>
2015-07-30 00:09:45 +0100

    added adaptive timestepping

--- src/shooting_solution.f90
+++ src/shooting_solution.f90
@@ -41,8 +41,8 @@ program stellarstructure
   yinner(1) = 0.0
   youter(1) = radiusstar
 
-  yinner(2) = pressurec/PSUN 
-  youter(2) = masst/(radiusstar**2) !((2.0*G)/(3*0.034))*(MSUN/(RSUN**2))*(masst/(radiusstar**2)) !from eddington approx
+  yinner(2) = pressurec
+  youter(2) = ((2.0*G)/(3*0.034))*(masst/(radiusstar**2)) !from eddington approx
 
   !unitinner header
   write(unitinner,*) "! inner stellar structure"
@@ -70,10 +70,10 @@ program stellarstructure
     double precision, dimension(size(y)) :: question4rkfunc
 
     !question4rkfunc(1) = (3.0/(4.0*PI))*((pressurec**(1/ratiospecificheat))/densityc)*DEXP(-y(2)/ratiospecificheat)
-    !question4rkfunc(2) = - (G/(4.0*PI))*x*(y(1)**(-4.0/3.0))*DEXP(-y(2))
+    !question4rkfunc(2) = - (G/(4.0*PI))*x*DEXP(-y(2))*(y(1)**(-4.0/3.0))
 
-    question4rkfunc(1) = (1/(4*PI))*(1/(y(1)**2))*(1/DENSUN)*((y(2))**(-1/ratiospecificheat)) !(1/(4.0*PI))*(1/(y(1)**2))*(1/densityc)*((y(2))**(-1/ratiospecificheat))
-    question4rkfunc(2) = -((3.0*opacity)/(8*PI))*(MSUN/(RSUN**2))*(x/(y(1)**4))  !-(G/(4.0*PI))*(x/(y(1)**4))*((MSUN**2)/(pressurec*RSUN**4))
+    question4rkfunc(1) = (1/(4.0*PI))*(1/(y(1)**2))*(1/densityc)*((y(2)/pressurec)**(-1/ratiospecificheat))
+    question4rkfunc(2) = -(G/(4.0*PI))*(x/(y(1)**4))
 
   end function
 
--- src/solvers.f90
+++ src/solvers.f90
@@ -22,8 +22,10 @@ subroutine rk4step (f,x,y,dx)
 
   k1 = f(x,y)
   k2 = f(x + (dx/2.0),y + (dx/2.0)*k1)
-  k3 = f(x + (dx/2.0),y + (dx/2.0)*k2) !appears to be failing here despite the others being secable
+  k3 = f(x + (dx/2.0),y + (dx/2.0)*k2) 
   k4 = f(x + dx,y + dx*k3)
+
+  !print *, k1
 
 
 
@@ -64,31 +66,47 @@ subroutine shootingmethod(f,xar,yinner,youter,unitinner,unitouter)
     end subroutine
   end interface
   double precision :: yinner(:),youter(:)
+  double precision :: pyinner(SIZE(yinner)), pyouter(SIZE(youter))
   double precision :: xar(3)
   double precision :: xinner, xouter
 
   double precision :: dxinner, dxouter
+
+  double precision :: maxchange = 0.1
 
   integer :: unitinner,unitouter
 
   xinner = xar(1)
   xouter = -xar(3)
 
-  dxinner = 0.001
-  dxouter = 0.001
+  dxinner = 1.0D20
+  dxouter = 1.0D20
 
   do while (xinner.lt.xar(2) .or. xouter.lt.-xar(2))
 
     if (xinner.lt.xar(2)) then
+ 
+      pyinner = yinner
+
       call rk4step (f,xinner,yinner,dxinner)
       write(unitinner,*) xinner, yinner
       flush(unitinner)
+! this adaptive stepping need a heck of alot of work
+      dxinner = MINVAL(maxchange*DABS(pyinner/(1.0D-10 + (yinner - pyinner))))
+
     end if 
 
     if (xouter.lt.-xar(2)) then
+
+      pyouter = youter
+
       call rk4step (fouter,xouter,youter,dxouter)
       write(unitouter,*) -xouter, youter
       flush(unitouter)
+
+      dxouter = MINVAL(maxchange*DABS(pyouter/(1.0D-10 + (youter - pyouter))))
+      print *, dxouter
+
     end if
 
     !stop

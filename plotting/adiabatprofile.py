911bb142370bea3d72fda96884599839b9447c1d
Elliot Lynch <elliotlynch@Elliots-MBP.default>
2015-08-20 17:50:19 +0100

    removed junk and fixed shooting_solution binary

--- plotting/adiabatprofile.py
+++ plotting/adiabatprofile.py
@@ -12,8 +12,8 @@ gamma = 5./3.
 
 #put in temperature and luminosity later
 mass_max = data[-1,0]
-radiusvar_max = data[-1,1]
-pressurevar_max = data[0,2]
+#radiusvar_max = data[0,1]
+#pressurevar_max = data[0,2]
 #tmax = data[0,3]
 #lummax =  data[-1,4]
 
@@ -24,25 +24,27 @@ print 'Stellar Parameters'
 print
 print 'Surface'
 #print 'Temperature: ', Temperature[-1]
-print 'Radius: ', radiusvar_max
+#print 'Radius: ', radiusvar_max
 print
 print 'Core'
 #print 'Temperature: ', Temperature[0]
-print 'Pressure: ', pressurevar_max
+#print 'Pressure: ', pressurevar_max
 print
 #print 'Error'
 #print 'Pressure: ', np.abs((inner[-1,1] - outer[0,1])/inner[-1,1])
 #print 'Radius: ',  np.abs((inner[-1,2] - outer[0,2])/inner[-1,2])
 
+data[:,1] = np.where(data[:,1]>1.0,1.0,data[:,1])
+data[:,2] = np.where(data[:,2]>1.0,1.0,data[:,2])
 
-
-pb.plot(data[:,0]/mass_max,data[:,1]/radiusvar_max,'k',linewidth=2.0,label='Normalised R')
-pb.plot(data[:,0]/mass_max,data[:,2]/pressurevar_max,'k:',linewidth=2.0,label='Normalised P')
+pb.plot(data[:,0]/mass_max,data[:,1],'k',linewidth=2.0,label='Normalised R')
+pb.plot(data[:,0]/mass_max,data[:,2],'k:',linewidth=2.0,label='Normalised P')
 #pb.plot(data[:,0]/mass_max,data[:,3]/tmax,'k--',linewidth=2.0,label='Normalised T')
 #pb.plot(data[:,0]/mass_max,data[:,4]/lummax,'k-.',linewidth=2.0,label='Normalised L')
 #pb.plot(data[:,0]/mass_max,linewidth=2.0,label='Normalised T^4')
 
 pb.xlabel('Normalised Mass')
+pb.ylim((0.0,1.0))
 
 pb.legend(loc='best')
 pb.show()
--- src/shooting_solution.f90
+++ src/shooting_solution.f90
@@ -7,7 +7,7 @@ program shootingsolution
   implicit none
 
   double precision :: xar(3)
-  double precision :: yinner(4), youter(4)
+  double precision :: yinner(4), youter(4), yparam(4)
   integer :: unitinner = 101,unitouter = 102
   integer :: unitinput = 103
 
@@ -25,17 +25,9 @@ program shootingsolution
   xar(2) = massm
   xar(3) = masst
 
-  yinner(1) = 0.0
-  youter(1) = radiusstar
+  yparam = (/radiusstar,pressurec,tempc,luminosity0/)
 
-  yinner(2) = pressurec
-  youter(2) = ((2.0*G)/(3.0*0.034))*(masst/(radiusstar**2.0))*(MSUN/(RSUN**2.0)) !from eddington approx
-
-  yinner(3) = tempc
-  youter(3) = ((luminosity0/(4.0*PI*STBOLTZ*(radiusstar**2)))*(LSUN/(RSUN**2.0)))**(1.0/4.0)
-
-  yinner(4) = 0.0
-  youter(4) = luminosity0
+  call question5boundcond(xar,yinner,youter,yparam)
 
   if (stype.eq.0 .or. stype.eq.1) then
     !unitinner header
@@ -56,12 +48,35 @@ program shootingsolution
   end if
 
   !run solver
-  call shootingmethod(question5rkfunc,question5innerboundary,xar,yinner,youter,unitinner,unitouter,stype) 
+  call shootingmethod(question5rkfunc,question5innerboundary,xar,yinner,youter,unitinner,unitouter,stype,1) 
 
   close(unitinner)
   close(unitouter)
 
   contains
+
+  subroutine question5boundcond(xar,yinner,youter,yparam)
+
+    double precision :: xar(3)
+    double precision :: yinner(:),youter(:)
+    double precision :: yparam(:)
+    double precision :: masst
+
+    masst = xar(3)
+
+    yinner(1) = 0.0
+    youter(1) = yparam(1)
+
+    yinner(2) = yparam(2)
+    youter(2) = (1.0/Gbar)*((2.0*G)/(3.0*opacity))*(masst/(yparam(1)**2.0))*(MSUN/(RSUN**2.0)) !from eddington approx
+
+    yinner(3) = yparam(3)
+    youter(3) = (1.0/MKELVIN)*(((yparam(4)/(4.0*PI*STBOLTZ*(yparam(1)**2)))*(LSUN/(RSUN**2.0)))**(1.0/4.0))
+
+    yinner(4) = 0.0
+    youter(4) = yparam(4)
+
+  end subroutine
 
   function question5rkfunc (x,y)
 
@@ -70,18 +85,17 @@ program shootingsolution
     double precision, dimension(size(y)) :: question5rkfunc
     double precision :: ppCNO_energypdc, density
 
-    !question4rkfunc(1) = (3.0/(4.0*PI))*((pressurec**(1/ratiospecificheat))/densityc)*DEXP(-y(2)/ratiospecificheat)
-    !question4rkfunc(2) = - (G/(4.0*PI))*x*DEXP(-y(2))*(y(1)**(-4.0/3.0))
-    
-    density = ((mmolecweight*y(2))/(RGAS*y(3)))
 
-    ppCNO_energypdc = ((0.25*(hmassfrac**2)*DEXP(-33.8 - 33.8*(y(3)/1.0D6)**(-1.0/3.0)) + & 
-   & 8.8D18*DEXP(-152.2*(y(3)/1.0D6)**(-1.0/3.0)))*((y(3)/1.0D6)**(-2.0/3.0)))*density
+    density = ((mmolecweight*y(2)*Gbar)/(RGAS*y(3)*MKELVIN))
+
+    ppCNO_energypdc = (0.25*(hmassfrac**2)*DEXP(-33.8*(y(3)**(-1.0/3.0))) + &
+   & 8.8D18*hmassfrac*DEXP(-152.28*(y(3)**(-1.0/3.0)))) * density * &
+   & (y(3)**(-2.0/3.0)) 
 
     question5rkfunc(1) = (1/(4.0*PI))*(1/(y(1)**2))*(1/density) * &
    & (MSUN/(RSUN**3))
-    question5rkfunc(2) = -(G/(4.0*PI))*(x/(y(1)**4))*((MSUN**2)/(RSUN**4))
-    question5rkfunc(3) = -((3*opacity*y(4))/(((16*PI)**2)*STBOLTZ*(y(1)**4)*(y(3)**3)))*((MSUN*LSUN)/(RSUN**4))
+    question5rkfunc(2) = -(G/(4.0*PI))*(x/(y(1)**4))*((MSUN**2)/(Gbar*(RSUN**4)))
+    question5rkfunc(3) = -((3*opacity*y(4))/(((16*PI)**2)*STBOLTZ*(y(1)**4)*((y(3)*MKELVIN)**3)))*((MSUN*LSUN)/(MKELVIN*(RSUN**4)))
     question5rkfunc(4) = ppCNO_energypdc*(MSUN/LSUN)
 
   end function
@@ -93,17 +107,26 @@ program shootingsolution
     double precision, dimension(size(y)) :: question5innerboundary
     double precision :: ppCNO_energypdc, density
 
-    density = (mmolecweight*y(2))/(RGAS*y(3))
+    density = (mmolecweight*y(2))*Gbar/(RGAS*y(3)*MKELVIN)
+   
+    ppCNO_energypdc = (0.25*(hmassfrac**2)*DEXP(-33.8*(y(3)**(-1.0/3.0))) + &
+   & 8.8D18*hmassfrac*DEXP(-152.28*(y(3)**(-1.0/3.0))) )* density * &
+   & (y(3)**(-2.0/3.0))
 
-    ppCNO_energypdc = ((0.25*(hmassfrac**2)*DEXP(-33.8 - 33.8*(y(3)/1.0D6)**(-1.0/3.0)) + & 
-   & 8.8D18*DEXP(-152.2*(y(3)/1.0D6)**(-1.0/3.0)))*((y(3)/1.0D6)**(-2.0/3.0)))*density*((y(3)/1.0D6)**(-2.0/3.0))
 
-    question5innerboundary(1) = ((3.0/(4.0*PI))*((RGAS*tempc)/(mmolecweight*pressurec))*x)**(1.0/3.0)
-    question5innerboundary(2) = pressurec - (2.0*PI/3.0)*G*(y(1)**2)*((mmolecweight*pressurec)/(RGAS*tempc))**2
-    question5innerboundary(3) = ((4.0*PI/3.0)*((mmolecweight*pressurec)/((RGAS*tempc)))**(1.0/3.0)) * &
-    & ((((12.0*opacity)/(STBOLTZ*(16*PI)**2)) * &
-    & (3.0*luminosity0*LSUN*((x*MSUN)**(-1.0/3.0)) - 1.5*ppCNO_energypdc*(2.0/3.0)))**(0.25)) * MSUN
+    !hacky way of ensuring r is sufficiently large
+    question5innerboundary(1) = 1.0D-4
+
+    x = ((4.0*PI/3.0)*density*question5innerboundary(1)**3)
+
+    question5innerboundary(2) = pressurec - (1.0/Gbar)*(2.0*PI/3.0)*G*(y(1)**2)*((mmolecweight*pressurec*Gbar)/(RGAS*tempc*MKELVIN))**2
+   
+    question5innerboundary(3) = (((tempc*MKELVIN)**4 - ((3.0*opacity)/(32.0*PI*STBOLTZ))*(1/density) * &
+   & ppCNO_energypdc*((x*MSUN)**(2.0/3.0)))**(0.25))/MKELVIN
+
     question5innerboundary(4) = ppCNO_energypdc*x*(MSUN/LSUN)
+
+   ! print *, question5innerboundary
 
   end function
 

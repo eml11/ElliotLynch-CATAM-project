934a415acb77b6e12c21daed57c43e6bec46778e
eml11 <eml11@ic.ac.uk>
2015-08-24 21:29:01 +0100

    Cleanup and Comment adiabatic_solution.f90

--- src/adiabatic_solution.f90
+++ src/adiabatic_solution.f90
@@ -1,4 +1,8 @@
-
+!
+! PROGRAM: adiabatic_solver  
+!
+!> Solves the equations of stellar structure using
+!! the adiabatic approximation for the eos.
 program adiabatic_solver
 
   use mod_shared
@@ -11,11 +15,12 @@ program adiabatic_solver
   integer :: unitinner = 101,unitouter = 102
   integer :: unitinput = 103
 
-  double precision :: opacity = 0.034
-  double precision :: mmolecweight = 1.0/1.625
+  double precision :: opacity
+  double precision :: mmolecweight
 
   call readinputfile (unitinput,'star.inp')
 
+  !open output files for writing
   open(unit=unitinner,FILE='stellarstructure_inner.txt')
   open(unit=unitouter,FILE='stellarstructure_outer.txt')
 
@@ -32,15 +37,14 @@ program adiabatic_solver
   youter(1) = radiusstar
 
   yinner(2) = pressurec
-  youter(2) = (1.0/Gbar)*(((2.0*G)/(3.0*opacity))*(masst/(radiusstar**2.0))*(MSUN/(RSUN**2.0))) !from eddington approx
+  youter(2) = (1.0/Gbar)*(((2.0*G)/(3.0*opacity))*(masst/(radiusstar**2.0))*(MSUN/(RSUN**2.0)))
 
-  !unitinner header
+  !write unitinner header
   write(unitinner,*) "! inner stellar structure"
   write(unitinner,*) "Mass Radius P"
   flush(unitinner) !not sure why this is suddenly necessary
 
-
-  !unitouter header
+  !write unitouter header
   write(unitouter,*) "! outer stellar structure"
   write(unitouter,*) "Mass Radius P"
   flush(unitouter)
@@ -48,20 +52,23 @@ program adiabatic_solver
   !run solver
   call shootingmethod(question4rkfunc,question4innerboundary,xar,yinner,youter,unitinner,unitouter,stype,1) 
 
+  !close output files
   close(unitinner)
   close(unitouter)
 
   contains
 
+  !> Derivatives of the stellar variables with respect to mass.
+  !! Used in Runge-Kutta integration
+  !! @param x mass indepenent variable
+  !! @param y array of dependent variables
+  !! @return question4rkfunc array of derivatives of y wrt x
   function question4rkfunc (x,y)
 
     double precision :: x
     double precision :: y(:)  !again possibly bad practice
     double precision, dimension(size(y)) :: question4rkfunc
     double precision :: density 
-
-    !question4rkfunc(1) = (3.0/(4.0*PI))*((pressurec**(1/ratiospecificheat))/densityc)*DEXP(-y(2)/ratiospecificheat)
-    !question4rkfunc(2) = - (G/(4.0*PI))*x*DEXP(-y(2))*(y(1)**(-4.0/3.0))
 
     density = ((mmolecweight*pressurec*Gbar)/(RGAS*tempc*MKELVIN))*((y(2)/pressurec)**(1.0/ratiospecificheat))
 
@@ -70,6 +77,11 @@ program adiabatic_solver
 
   end function
 
+  !> Analytic solutions to the linearised equation for the stellar variables
+  !! near the core. Gives stellar variables at R=1.0D-4 
+  !! @param x mass indepenent variable
+  !! @param y array of dependent variables
+  !! @return question4innerboundary array of dependent variables at R=1.0D-4
   function question4innerboundary (x,y)
 
     double precision :: x
@@ -83,7 +95,6 @@ program adiabatic_solver
 
     x = (1.0/MSUN)*((4.0*PI/3.0)*density*(question4innerboundary(1)*RSUN)**3)
 
-    !question4innerboundary(1) = (1.0/RSUN)*(((3.0/(4.0*PI*density))*x*MSUN)**(1.0/3.0))
     question4innerboundary(2) = pressurec - (1.0/Gbar)*(2.0*PI/3.0)*G*((question4innerboundary(1)*RSUN)**2)*density**2
 
   end function
